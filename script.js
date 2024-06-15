3c30c6ae803655c1c18808ce748ef67991aeb11e
Sayan Chandra <146355596+schandra2609@users.noreply.github.com>
2024-06-15 21:04:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container');
+    const yes = document.getElementById('yes');
+    const no = document.getElementById('no');
+    const audioWow = document.getElementById('wow');
+    const audioAayein = document.getElementById('aayein');
+    const audioMc  = document.getElementById('madar');
+
+    const contRect = container.getBoundingClientRect();
+    const noRect = no.getBoundingClientRect();
+
+    let counter = 0;
+
+
+    function playAudio(audio) {
+        audio.play();
+    };
+
+    function moveButton() {
+        let x = Math.floor(Math.random() * (contRect.width - noRect.width));
+        let y = Math.floor(Math.random() * (contRect.height - noRect.height - 400));
+        no.style.left = `${x}px`;
+        no.style.top = `${y}px`;
+    };
+
+    yes.addEventListener('click', function() {
+        playAudio(audioWow);
+        setTimeout(function() {
+            alert('Yeeee!! Thank You 😍😍😍');
+        }, 100);
+        no.style.left = `auto`;
+        no.style.top = `300px`;
+        counter = 0;
+    });
+
+    no.addEventListener('mouseenter', function() {
+        moveButton();
+        counter++;
+        counter <= 10 ? playAudio(audioAayein) : playAudio(audioMc);
+    });
+
+    no.addEventListener('click', function() {
+        moveButton();
+        counter++;
+        counter <= 10 ? playAudio(audioAayein) : playAudio(audioMc);
+    });
+});
